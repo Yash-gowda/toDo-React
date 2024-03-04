4a932b0e66048e342ae9927d30eb8d4ee2a1ae58
Yash-gowda <yashwanth.gowda.b.c@gmail.com>
2024-03-04 13:18:43 -0500

    added some comments

--- src/TasksContext.js
+++ src/TasksContext.js
@@ -2,11 +2,13 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const TasksContext = createContext();
 
+// Custom hook for TasksContext
 export const useTasks = () => useContext(TasksContext);
 
 export const TasksProvider = ({ children }) => {
+    //State hook for managing the tasks
     const [tasks, setTasks] = useState(() => {
-        const savedTasks = localStorage.getItem('tasks');
+        const savedTasks = localStorage.getItem('tasks'); // An attempt to retrieve tasks from the local storage
         return savedTasks ? JSON.parse(savedTasks) : [];
     });
 
@@ -14,10 +16,10 @@ export const TasksProvider = ({ children }) => {
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }, [tasks]);
 
+    // value provided to consumers of the context.
     const value = {
         tasks,
         setTasks,
     };
-
     return <TasksContext.Provider value={value}>{children}</TasksContext.Provider>;
 };
--- src/useApi.js
+++ src/useApi.js
@@ -1,8 +1,14 @@
 import { useState } from 'react';
 
+//custom hook to initialize and manage API requests
 export const useApi = (initialData) => {
+    
+    //state hook for storing data returned by API
     const [data, setData] = useState(initialData);
+    // State hook for tracking the loading status of the API request.
     const [loading, setLoading] = useState(false);
+
+    // State hook for storing any errors that occur during the API request. Set to 'null' in the beginning
     const [error, setError] = useState(null);
 
     const request = (thunkFunction) => {
@@ -13,6 +19,5 @@ export const useApi = (initialData) => {
             .catch((err) => setError(err))
             .finally(() => setLoading(false));
     };
-
     return { data, loading, error, request };
 };
